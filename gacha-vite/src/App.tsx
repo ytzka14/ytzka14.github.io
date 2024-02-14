cb07b483805bbb3b4ddce7ac3f4884132b932916
Minhyeong Kim <ytzka14@snu.ac.kr>
2024-02-14 16:21:11 +0900

    gacha-vite rarity 6 stack probability bug fixed

--- gacha-vite/src/App.tsx
+++ gacha-vite/src/App.tsx
@@ -4,10 +4,15 @@ import gachaJson from './assets/gacha.json';
 import Display from "./Display";
 import './App.css';
 
+let sixStack = 0;
+let tenStack = 0;
+let dispSixProb = 0.02;
+let dispFiveProb = 0.08;
+let dispFourProb = 0.5;
+let dispThreeProb = 0.4;
+
 function App() {
 	const [dispList, setDispList] = useState<charType[]>([])
-	let sixStack = 0;
-	let tenStack = 0;
 	const [rarityCnt, setRarityCnt] = useState([0, 0, 0, 0]);
 
 	const gachaData: charType[] = gachaJson.map((d) => {return {name: d.name, rarity: Number(d.rarity), pickup: Boolean(d.pickup)}});
@@ -46,6 +51,10 @@ function App() {
 		for (let i = 0; i < x; i++) {
 			const rng = Math.random();
 			if (sixStack < 50 && tenStack != 9) {
+				dispSixProb = 0.02;
+				dispFiveProb = 0.08;
+				dispFourProb = 0.5;
+				dispThreeProb = 0.4;
 				if (rng < 0.02) {
 					gachaResult.push(getChar(6));
 					addCnt[3]++;
@@ -68,6 +77,10 @@ function App() {
 					tenStack = Math.min(10, tenStack+1);
 				}
 			} else if (tenStack == 9) {
+				dispSixProb = 0.02;
+				dispFiveProb = 0.98;
+				dispFourProb = 0;
+				dispThreeProb = 0;
 				if (rng < 0.02) {
 					gachaResult.push(getChar(6));
 					addCnt[3]++;
@@ -83,7 +96,11 @@ function App() {
 				const sixProb = 0.02 * (sixStack - 49);
 				const fiveProb = (1 - sixProb) * 8 / 98;
 				const fourProb = (1 - sixProb) * 50 / 98;
-				if (rng < sixProb) {
+				dispSixProb = sixProb;
+				dispFiveProb = fiveProb;
+				dispFourProb = fourProb;
+				dispThreeProb = 1 - dispSixProb - dispFiveProb - dispFourProb;
+				if (rng <= sixProb) {
 					gachaResult.push(getChar(6));
 					addCnt[3]++;
 					sixStack = 0;
@@ -120,7 +137,9 @@ function App() {
 				<span>가챠 회수: {sum(rarityCnt)}회</span><br/>
 				<span>소모 합성옥: {sum(rarityCnt) * 600}개</span><br/>
 				<span>오리지늄으로 변환시: {sum(rarityCnt) * 600 / 180}개</span><br/>
-				<span>레어도별 등장 회수: 6성 {rarityCnt[3]}회, 5성 {rarityCnt[2]}회, 4성 {rarityCnt[1]}회, 3성 {rarityCnt[0]}회</span>
+				<span>레어도별 등장 회수: 6성 {rarityCnt[3]}회, 5성 {rarityCnt[2]}회, 4성 {rarityCnt[1]}회, 3성 {rarityCnt[0]}회</span><br/>
+				<span>6성 스택: {sixStack}회 미등장</span><br/>
+				<span>레어도별 등장 확률: 6성 {dispSixProb}%, 5성 {dispFiveProb}%, 4성 {dispFourProb}%, 3성 {dispThreeProb}%</span>
 			</div>
 		</>
 	)
